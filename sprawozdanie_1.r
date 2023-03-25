1a440a533d6fdeff981451e54801e5328319f928
Michał <86123830+mweuro@users.noreply.github.com>
2023-03-25 18:59:22 +0100

    Zostało zad 9 i 11

--- sprawozdanie_1.r
+++ sprawozdanie_1.r
@@ -135,51 +135,119 @@ atmosphere <- function(fltr, grp, dt){
   dt <- deparse(substitute(dt))
   df1 <- filter(df, df[[fltr]] == grp)
   df2 <- data.frame(as.factor(df1[[dt]]))
+  colnames(df2) <- ''
   return(df2)
 }
+##pierwszy okres ankiet - A1
+##likert - cała grupa
+all_employees <- data.frame(as.factor(df[['A1']]))
+colnames(all_employees) <- 'all'
+all_employees_likert <- likert(all_employees)
+all_employees_summary <- summary(all_employees_likert)
+bar_all_employees <- plot(all_employees_likert,
+                          type = 'bar')
+bar_all_employees
+den_all_employees <- likert.density.plot(all_employees_likert, 
+                                facet = TRUE, bw = 0.5)
+den_all_employees
 
-##kobiety
+##likert - kobiety i mężczyźni
 women <- atmosphere(P, K, A1)
-women1 <- likert(women)
-summary(women1)
-plot(women1, type = 'bar')
-plot(women1, type = 'density')
-
-##mężczyźni
+colnames(women) <- 'women'
 men <- atmosphere(P, M, A1)
-men1 <- likert(men)
-summary(men1)
-plot(men1, type = 'bar')
-plot(men1, type = 'density')
+colnames(men) <- 'men'
+both <- merge(men, women, 
+           by = 0, all = TRUE)[-1]
+both_likert <- likert(both)
+both_summary <- summary(both_likert)
+bar_both <- plot(both_likert, type = 'bar')
+bar_both
+den_both <- likert.density.plot(both_likert, 
+                                facet = TRUE, bw = 0.5)
+den_both
 
-##dział zaopatrzenia
+##likert - wszystkie działy
 inv <- atmosphere(D, Z, A1)
-inv1 <- likert(inv)
-summary(inv1)
-plot(inv1, type = 'bar')
-plot(inv1, type = 'density')
-
-##dział produkcyjny
+colnames(inv) <- 'inv'
 prod <- atmosphere(D, P, A1)
-prod1 <- likert(prod)
-summary(prod1)
-plot(prod1, type = 'bar')
-plot(prod1, type = 'density')
-
-##dział sprzedaży
+colnames(prod) <- 'prod'
 sales <- atmosphere(D, S, A1)
-sales1 <- likert(sales)
-summary(sales1)
-plot(sales1, type = 'bar')
-plot(sales1, type = 'density')
-
-##dział obsługi kadrowo-płacowej
+colnames(sales) <- 'sales'
 serv <- atmosphere(D, O, A1)
-serv1 <- likert(serv)
-summary(serv1)
-plot(serv1, type = 'bar')
-plot(serv1, type = 'density')
+colnames(serv) <- 'serv'
+departments <- merge(inv, prod, by = 0,
+                     all = TRUE)[-1]
+departments <- merge(departments, sales,
+                     by = 0,
+                     all = TRUE)[-1]
+departments <- merge(departments, serv,
+                     by = 0,
+                     all = TRUE)[-1]
+departments[1:4] <- lapply(departments[1:4], factor,
+                      levels = -2:2)
+departments_likert <- likert(departments[, c(1:4)])
+departments_summary <- summary(departments_likert)
+bar_departments <- plot(departments_likert, type = 'bar')
+bar_departments
+den_departments <- likert.density.plot(departments_likert,
+                        facet = TRUE, bw = 0.5)
+den_departments
 
+
+##drugi okres ankiet - A2
+##likert - cała grupa
+all_employees2 <- data.frame(as.factor(df[['A2']]))
+colnames(all_employees2) <- 'all'
+all_employees2_likert <- likert(all_employees2)
+all_employees2_summary <- summary(all_employees2_likert)
+bar_all_employees2 <- plot(all_employees2_likert,
+                          type = 'bar')
+bar_all_employees2
+den_all_employees2 <- likert.density.plot(all_employees2_likert, 
+                                         facet = TRUE, bw = 0.5)
+den_all_employees2
+
+##likert - kobiety i mężczyźni
+women2 <- atmosphere(P, K, A2)
+colnames(women2) <- 'women'
+men2 <- atmosphere(P, M, A2)
+colnames(men2) <- 'men'
+both2 <- merge(men2, women2, 
+              by = 0, all = TRUE)[-1]
+both2_likert <- likert(both2)
+both2_summary <- summary(both2_likert)
+bar_both2 <- plot(both2_likert, type = 'bar')
+bar_both2
+den_both2 <- likert.density.plot(both2_likert, 
+                                facet = TRUE, bw = 0.5)
+den_both2
+
+##likert - wszystkie działy
+inv2 <- atmosphere(D, Z, A2)
+colnames(inv2) <- 'inv'
+prod2 <- atmosphere(D, P, A2)
+colnames(prod2) <- 'prod'
+sales2 <- atmosphere(D, S, A2)
+colnames(sales2) <- 'sales'
+serv2 <- atmosphere(D, O, A2)
+colnames(serv2) <- 'serv'
+departments2 <- merge(inv2, prod2, by = 0,
+                     all = TRUE)[-1]
+departments2 <- merge(departments2, sales2,
+                     by = 0,
+                     all = TRUE)[-1]
+departments2 <- merge(departments2, serv2,
+                     by = 0,
+                     all = TRUE)[-1]
+departments2[1:4] <- lapply(departments2[1:4], factor,
+                           levels = -2:2)
+departments2_likert <- likert(departments2[, c(1:4)])
+departments2_summary <- summary(departments2_likert)
+bar_departments2 <- plot(departments2_likert, type = 'bar')
+bar_departments2
+den_departments2 <- likert.density.plot(departments2_likert,
+                                       facet = TRUE, bw = 0.5)
+den_departments2
 
 #ZAD 7
 library(binom)
@@ -188,7 +256,19 @@ alpha <- 0.05
 ##Gotowa funkcja z pakietu binom
 clopper1 <- function(alpha, poll){
   tbl <- table(poll)
-  positive <- tbl[['1']] + tbl[['2']]
+  if((1 %in% poll) == FALSE){
+    positive <- tbl[['2']]
+  }
+  else if((2 %in% poll) == FALSE){
+    positive <- tbl[['1']]
+  }
+  else if(((1 %in% poll) == FALSE) & 
+          ((2 %in% poll) == FALSE)){
+    positive <- 0
+  }
+  else{
+    positive <- tbl[['1']] + tbl[['2']]
+  }
   all <- sum(tbl)
   confint1 <- binom.confint(positive, all,
                             conf.level = 1 - alpha,
@@ -198,39 +278,78 @@ clopper1 <- function(alpha, poll){
 }
 
 ##Ręczne policzenie
-clopper2 <- function(poll){
-  colnames(poll) <- 'vote'
-  vote <- poll$vote
-  vote <- replace(vote, vote == 2, 1)
-  vote <- replace(vote, vote == -1, 0)
-  vote <- replace(vote, vote == -2, 0)
-  vote <- replace(vote, vote == 11, 1)
-  poll2 <- data.frame(vote)
-  colnames(poll2) <- 'vote'
-  return(poll2)
+clopper2 <- function(alpha, poll){
+  tbl <- table(poll)
+  if((1 %in% poll) == FALSE){
+    positive <- tbl[['2']]
+  }
+  else if((2 %in% poll) == FALSE){
+    positive <- tbl[['1']]
+  }
+  else if(((1 %in% poll) == FALSE) & 
+          ((2 %in% poll) == FALSE)){
+    positive <- 0
+  }
+  else{
+  positive <- tbl[['1']] + tbl[['2']]
+  }
+  all <- sum(tbl)
+  left <- qbeta(alpha/2, positive, all - positive + 1)
+  right <- qbeta(1 - alpha/2, positive + 1, all - positive)
+  return(c(left, right))
 }
 
 ###Cała grupa
-inter1 <- clopper1(alpha, df$A1)
+inter1 <- clopper1(alpha, df$W1)
 inter1
+inter1.2 <- clopper2(alpha, df$W1)
+inter1.2
 
 ###Dział zaopatrzenia
-inter2 <- clopper1(alpha, inv)
+inv_salary <- atmosphere(D, Z, W1)
+inter2 <- clopper1(alpha, inv_salary)
 inter2
+inter2.2 <- clopper2(alpha, inv_salary)
+inter2.2
 
 ###Dział produkcyjny
-inter3 <- clopper1(alpha, prod)
+prod_salary <- atmosphere(D, P, W1)
+inter3 <- clopper1(alpha, prod_salary)
 inter3
+inter3.2 <- clopper2(alpha, prod_salary)
+inter3.2
 
 ###Dział sprzedaży
-inter4 <- clopper1(alpha, sales)
+sales_salary <- atmosphere(D, S, W1)
+inter4 <- clopper1(alpha, sales_salary)
 inter4
+inter4.2 <- clopper2(alpha, sales_salary)
+inter4.2
 
 ###Dział obsługi kadrowo-płacowej
-inter5 <- clopper1(alpha, serv)
+serv_salary <- atmosphere(D, O, W1)
+inter5 <- clopper1(alpha, serv_salary)
 inter5
+inter5.2 <- clopper2(alpha, serv_salary)
+inter5.2
+
+###Pracownicy na zwykłych stanowiskach
+emp_salary <- atmosphere(S, 0, W1)
+inter6 <- clopper1(alpha, emp_salary)
+inter6
+inter6.2 <- clopper2(alpha, emp_salary)
+inter6.2
+
+###Pracownicy na stanowiskach kierowniczych
+mgr_salary <- atmosphere(S, 1, W1)
+inter7 <- clopper1(alpha, mgr_salary)
+inter7
+inter7.2 <- clopper2(alpha, mgr_salary)
+inter7.2
 
 ##CZĘŚĆ 3##
+
+## zad 8
 binomial <- function(n, p, N){
   bin <- c()
   for(i in 1:N){
@@ -241,15 +360,74 @@ binomial <- function(n, p, N){
   return(bin)
 }
 
-comp_hist <- function(n, p, N){
+comp_hist <- function(n, p, N, leg = TRUE){
   # funkcja rbinom
   df1 <- data.frame(x = c(rbinom(N, n, p), binomial(n, p, N)),
                     group = as.factor(c(rep(1, N), rep(2, N))))
   plt1 <- ggplot(df1, aes(x = x, fill = group)) +
-    geom_histogram(alpha = 0.5, binwidth = 1,
-                   position = 'identity')
-  plt1
-  # return(df1)
+    geom_histogram(aes(y = after_stat(density)), 
+                   alpha = 0.3, 
+                   binwidth = 1,
+                   position = 'identity') +
+    labs(y = 'p', fill = 'Użyta \nfunkcja')
+  if(leg){
+    plt1 +
+      scale_fill_discrete(labels = c("rbinom", 
+                                     "ręcznie")) +
+      theme(legend.key.size = unit(5, 'mm'),
+            legend.text = element_text(size=5))
+  }
+  else{
+    plt1 + theme(legend.position = 'none')
+  }
 }
+comp_hist(500, 0.3, 500)
+comp_hist(500, 0.3, 5000)
+
+comp_cdf <- function(n, p, N, leg = TRUE){
+  # funkcja rbinom
+  df1 <- data.frame(x = c(rbinom(N, n, p), binomial(n, p, N)),
+                    group = as.factor(c(rep(1, N), rep(2, N))))
+  plt1 <- ggplot(df1, aes(x = x, col = group)) +
+    stat_ecdf() +
+    labs(y = 'p', col = 'Użyta \nfunkcja')
+  if(leg){
+    plt1 +
+      scale_color_discrete(labels = c("rbinom", 
+                                     "ręcznie")) +
+      theme(legend.key.size = unit(5, 'mm'),
+            legend.text = element_text(size=5))
+  }
+  else{
+    plt1 + theme(legend.position = 'none')
+  }
+}
+comp_cdf(500, 0.3, 500)
+comp_cdf(500, 0.3, 5000)
+## zad 9 (ma Zosia)
+
 ##CZĘŚĆ 4##
+
+## zad 10, 11
+
+# a
+x_a <- df$P
+x_a_success <- length(x_a[x_a == 'K'])
+x_a_n <- length(x_a)
+test_a <- binom.test(x_a_success, x_a_n, p = 0.5, conf.level = 0.95)
+
+# b
+x_b <- df %>% group_by(W1) %>% count() 
+x_b_success <- (x_b[4, 'n'] + x_b[3, 'n'])$n
+x_b_n <- length(df$W1)
+test_b <- binom.test(x_b_success, x_b_n, p = 0.8, 
+                     conf.level = 0.95,
+                     alternative = 'greater')
+
+# c
+
+# d
+
+# e
+
 
